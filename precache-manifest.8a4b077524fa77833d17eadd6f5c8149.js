2a2a8a6c14e4c937b0215dccfdfcc90608f268bb
USER <user@USERs-MacBook-Pro.local>
2022-08-21 22:12:41 -0400

    Updates

---
+++ precache-manifest.8a4b077524fa77833d17eadd6f5c8149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dace720f71c27e241f318855be91b589",
+    "url": "/Recipes/index.html"
+  },
+  {
+    "revision": "241df4738204ab02fcfe",
+    "url": "/Recipes/static/css/main.b772f09e.chunk.css"
+  },
+  {
+    "revision": "99361c5bf318aceb0273",
+    "url": "/Recipes/static/js/2.10c6fc4b.chunk.js"
+  },
+  {
+    "revision": "ab8f792973d32f9b93c607ee81282492",
+    "url": "/Recipes/static/js/2.10c6fc4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "241df4738204ab02fcfe",
+    "url": "/Recipes/static/js/main.c176b86d.chunk.js"
+  },
+  {
+    "revision": "01399b9766f13a59da36",
+    "url": "/Recipes/static/js/runtime-main.f7d53b18.js"
+  }
+]);
